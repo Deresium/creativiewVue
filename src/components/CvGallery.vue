7740744cc8ce7295e6b5d4cb8d195ba694d4982e
deresium <dimitri.steinbusch@hotmai.com>
2020-02-16 13:53:05 +0100

    first commit creatiview

---
+++ src/components/CvGallery.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="gallery">
+        <p>Gallery</p>
+        <p>{{ gallery.getDescription() }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue, Prop} from 'vue-property-decorator'
+    import Gallery from "@/models/Gallery";
+    @Component
+    export default class CvGallery extends Vue{
+        @Prop() private gallery!: Gallery;
+    }
+</script>
+
+<style scoped>
+    .gallery{
+        background-color: #222222;
+        color: #DDDDDD;
+    }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    outputDir: '../creativiewNode/public'
+};
