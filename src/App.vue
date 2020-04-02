ddcbe1f6a899807544e3b1afe1981743ea8a7592
deresium <dimitri.steinbusch@hotmai.com>
2020-04-02 21:53:13 +0200

    Creatiview: beginning

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,16 @@
 <template>
-  <div>
-    <CvGallery v-for="(gallery, i) in galleries" :gallery="gallery" :key="i"></CvGallery>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
-<script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import CvGallery from "@/components/CvGallery.vue";
-  import Gallery from "@/models/Gallery";
-  import axios from "@/axios/axios-creatiview"
-
-  @Component({
-    components: {
-      CvGallery
-    },
-  })
-  export default class App extends Vue {
-    private galleries: Gallery[] = [];
-
-    created() {
-      axios.get('/galleries').then(response => {
-        response.data.forEach((galleryJson: {id: number; name: string; description: string}) => {
-          const gallery = new Gallery(galleryJson.id, galleryJson.name, galleryJson.description);
-          this.galleries.push(gallery);
-        });
-      });
-    }
-  }
-</script>
-
 <style>
+    #app{
+        width: 100vw;
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img src="../assets/textSlogan.svg" alt="logoWithText"/>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Home extends Vue {
+    }
+</script>
+
+<style scoped>
+    div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
